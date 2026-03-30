cf7e9501c18e2671c08750c52d746eb685e531d3
ainis997 <235386176+ainis997@users.noreply.github.com>
2026-03-30 13:40:32 +0300

    Pertvarkomas studentų galutinių balų tvarkymas.

--- src/strukturu_metodai.cpp
+++ src/strukturu_metodai.cpp
@@ -7,37 +7,37 @@
 #include "strukturos_konstantos.h"
 #include "isvesties_pagalb_fjos.h" // void rikiavimas(...)
 
-void Studentas::apsk_vid()
-{
-    if (pazymiai.size() == 0)
-        rezas_vid = egzo_rezas * 0.6;
-    int pazymiu_suma = 0;
-    for (auto paz : pazymiai)
-        pazymiu_suma += paz;
-    rezas_vid = ((pazymiu_suma * 1.0) / (pazymiai.size() * 1.0)) * 0.4 + (egzo_rezas * 0.6);
-}
+// void Studentas::apsk_vid()
+// {
+//     if (pazymiai.size() == 0)
+//         rezas_vid = egzo_rezas * 0.6;
+//     int pazymiu_suma = 0;
+//     for (auto paz : pazymiai)
+//         pazymiu_suma += paz;
+//     rezas_vid = ((pazymiu_suma * 1.0) / (pazymiai.size() * 1.0)) * 0.4 + (egzo_rezas * 0.6);
+// }
 
-void Studentas::apsk_med()
-{
-    if (pazymiai.size() == 0)
-        return;
-    Container(int) visi_pazymiai = pazymiai;
-    visi_pazymiai.push_back(egzo_rezas);
-    rikiavimas(visi_pazymiai); // sort(..) surikiuoja visi_pazymiai vektorių did. tvarka
-    int visu_pazymiu_sk = visi_pazymiai.size();
-    // vidurinės(-ių) reikšmės(-ių) radimas UNIVERSALIU būdu (tinka ir vector, ir deque, ir list)
-    auto it = visi_pazymiai.begin();
-    if (visu_pazymiu_sk % 2 != 0)
-    {
-        rezas_med = *std::next(it, visu_pazymiu_sk / 2); // pastumiam iteratorių per pusę visu_pazymiu_sk — į visi_pazymiai vidurį
-    }
-    else
-    {
-        auto vid1 = std::next(it, (visu_pazymiu_sk / 2) - 1);
-        auto vid2 = std::next(vid1, 1); // pastumiam iteratorių dar per vieną, kad gautume antrąją viduriniąją reikšmę
-        rezas_med = (*vid1 + *vid2) / 2.0;
-    }
-}
+// void Studentas::apsk_med()
+// {
+//     if (pazymiai.size() == 0)
+//         return;
+//     Container(int) visi_pazymiai = pazymiai;
+//     visi_pazymiai.push_back(egzo_rezas);
+//     rikiavimas(visi_pazymiai); // sort(..) surikiuoja visi_pazymiai vektorių did. tvarka
+//     int visu_pazymiu_sk = visi_pazymiai.size();
+//     // vidurinės(-ių) reikšmės(-ių) radimas UNIVERSALIU būdu (tinka ir vector, ir deque, ir list)
+//     auto it = visi_pazymiai.begin();
+//     if (visu_pazymiu_sk % 2 != 0)
+//     {
+//         rezas_med = *std::next(it, visu_pazymiu_sk / 2); // pastumiam iteratorių per pusę visu_pazymiu_sk — į visi_pazymiai vidurį
+//     }
+//     else
+//     {
+//         auto vid1 = std::next(it, (visu_pazymiu_sk / 2) - 1);
+//         auto vid2 = std::next(vid1, 1); // pastumiam iteratorių dar per vieną, kad gautume antrąją viduriniąją reikšmę
+//         rezas_med = (*vid1 + *vid2) / 2.0;
+//     }
+// }
 
 void Programos_laikai::spausd_laikus()
 {
--- src/studentas.h
+++ src/studentas.h
@@ -1,40 +1,119 @@
 #include <string>
 #include <sstream>
+#include <iomanip>
+#include <fstream>
 
 #include "strukturos_konstantos.h"
 
 class Studentas
 {
 private:
-    std::string vardas;
-    std::string pavarde;
-    Container(int) pazymiai;
-    int egzo_rezas;
-    float rezas_vid;
-    float rezas_med;
+    std::string vardas_;
+    std::string pavarde_;
+    Container(int) pazymiai_;
+    int egzo_rezas_;
+    float rezas_;
+    enum class Galutinis : char
+    {
+        VID,
+        MED
+    };
+    Galutinis gal_;
+    // float rezas_vid;
+    // float rezas_med;
 
 public:
-    Studentas(std::string vardas, std::string pavarde) : vardas(vardas), pavarde(pavarde), egzo_rezas(0), rezas_vid(0), rezas_med(0) {}
+    Studentas(std::string vardas_, std::string pavarde_) : vardas_(vardas_), pavarde_(pavarde_), egzo_rezas_(0), rezas_(0) {}
+
     // gavikai / getteriai
-    inline float rezas_vid() const { return rezas_vid; }
-    inline float rezas_med() const { return rezas_med; }
+
+    inline float rezas() const { return rezas_; }
+
     // nustatytojai / setteriai
-    void set_egzo_rezas(int paz) { egzo_rezas = paz; }
-    void apsk_vid();
-    void apsk_med();
+
+    void set_egzo_rezas_(int paz) { egzo_rezas_ = paz; }
+    void apsk_galutini()
+    {
+        if (gal_ == Galutinis::MED)
+        {
+            if (pazymiai_.size() == 0)
+                return;
+            Container(int) visi_pazymiai = pazymiai_;
+            visi_pazymiai.push_back(egzo_rezas_);
+            rikiavimas(visi_pazymiai); // sort(..) surikiuoja visi_pazymiai vektorių did. tvarka
+            int visu_pazymiu_sk = visi_pazymiai.size();
+            // vidurinės(-ių) reikšmės(-ių) radimas UNIVERSALIU būdu (tinka ir vector, ir deque, ir list)
+            auto it = visi_pazymiai.begin();
+            if (visu_pazymiu_sk % 2 != 0)
+            {
+                rezas_ = *std::next(it, visu_pazymiu_sk / 2); // pastumiam iteratorių per pusę visu_pazymiu_sk — į visi_pazymiai vidurį
+            }
+            else
+            {
+                auto vid1 = std::next(it, (visu_pazymiu_sk / 2) - 1);
+                auto vid2 = std::next(vid1, 1); // pastumiam iteratorių dar per vieną, kad gautume antrąją viduriniąją reikšmę
+                rezas_ = (*vid1 + *vid2) / 2.0;
+            }
+        }
+        else
+        {
+            if (pazymiai_.size() == 0)
+                rezas_ = egzo_rezas_ * 0.6;
+            int pazymiu_suma = 0;
+            for (auto paz : pazymiai_)
+                pazymiu_suma += paz;
+            rezas_ = ((pazymiu_suma * 1.0) / (pazymiai_.size() * 1.0)) * 0.4 + (egzo_rezas_ * 0.6);
+        }
+    };
+    // void apsk_vid();
+    // void apsk_med();
+
     // kt metodai
-    std::istringstream &stud_skaitymas(std::istringstream &);
-    void stud_spausdinimas();
+
+    void skaityt_studenta(std::istringstream &srautas, std::string eil)
+    {
+        srautas.clear();  // išvalo srautą (išvalo nuo praeitos iteracijos likusį statusą); be šito, skaitytų tik pirmą eilutę iš viso failo! (neveiktų)
+        srautas.str(eil); // įstato naują stringą (eilutę) srautan
+
+        if (!(srautas >> vardas_ >> pavarde_))
+            throw std::invalid_argument("Netinkamas studento duomenu pavidalas.");
+
+        int temp; // laikinas kintamasis pažymių perdavimui
+        while (srautas >> temp)
+        {
+            pazymiai_.push_back(std::move(temp));
+        }
+
+        if (pazymiai_.empty())
+        {
+            throw std::invalid_argument("Nepavyko nuskaityti studento pazymiu.");
+        }
+        egzo_rezas_ = pazymiai_.back(); // paskutinis elementas — egzamino rezas_
+        pazymiai_.pop_back();           // ištrinam egzo rezą iš pažymių vektoriaus
+
+        // apsk_vid();
+        // apsk_med();
+
+        apsk_galutini();
+    }
+    void studento_spausd(std::ofstream &ras_failas)
+    {
+        ras_failas
+            << std::left << std::setw(20) << vardas_
+            << std::left << std::setw(25) << pavarde_
+            << std::setw(15) << std::fixed << std::setprecision(2) << rezas_
+            << '\n';
+    }
 };
 
-struct Studentas
-{
-    Container(int) pazymiai;
-    std::string vardas = "Vardas";
-    std::string pavarde = "Pavarde";
-    float rezas_vid = 0;
-    float rezas_med = 0;
-    int egzo_rezas = 0;
-    void apsk_vid(); // void, nes vid. ir med. reik iš anksto apskaičiuot (o ne spausd metu), nes pagal tai reik leist rūšiuot
-    void apsk_med();
-};
+// struct Studentas
+// {
+//     Container(int) pazymiai_;
+//     std::string vardas_ = "Vardas";
+//     std::string pavarde_ = "pavarde_";
+//     float rezas__vid = 0;
+//     float rezas__med = 0;
+//     int egzo_rezas__ = 0;
+//     void apsk_vid(); // void, nes vid. ir med. reik iš anksto apskaičiuot (o ne spausd metu), nes pagal tai reik leist rūšiuot
+//     void apsk_med();
+// };
