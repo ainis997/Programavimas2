2f20587b31e695ada8f628861d54f54373c801e3
ainis997 <235386176+ainis997@users.noreply.github.com>
2026-04-21 14:01:54 +0300

    Baigiama kurti abstrakti žmogaus klasė.

--- src/studentas.h
+++ src/studentas.h
@@ -6,16 +6,17 @@
 #include <iomanip>
 #include <fstream>
 
+#include "zmogus.h"
 #include "ivesties_pagalb_fjos.h"
 #include "isvesties_pagalb_fjos.h"
 #include "konstantos_kt_klases.h"
 #include "klaidu_valdymas.h"
 
-class Studentas
+class Studentas : public Zmogus
 {
 private:
-    std::string vardas_;
-    std::string pavarde_;
+    // std::string vardas_;
+    // std::string pavarde_;
     Container(int) pazymiai_;
     int egzo_rezas_;
     float rezas_vid_;
@@ -24,27 +25,20 @@ private:
 
 public:
     // nuskaitant
-    Studentas() : vardas_(""), pavarde_(""), egzo_rezas_(0), rezas_vid_(0), rezas_med_(0) {}
+    Studentas() : Zmogus(), egzo_rezas_(0), rezas_vid_(0), rezas_med_(0) {}
     // surasant rankiniu budu
-    Studentas(std::string vardas, std::string pavarde) : vardas_(vardas), pavarde_(pavarde), egzo_rezas_(0), rezas_vid_(0), rezas_med_(0) {}
+    Studentas(std::string vardas, std::string pavarde) : Zmogus(vardas, pavarde), egzo_rezas_(0), rezas_vid_(0), rezas_med_(0) {}
 
-    // destruktorius
-    ~Studentas()
-    {
-        pazymiai_.clear();
-    }
     // kopijavimo konstr [Studentas x(y) / Studentas x = y]
     Studentas(const Studentas &kitas)
-        : vardas_(kitas.vardas_),
-          pavarde_(kitas.pavarde_),
+        : Zmogus(kitas),
           pazymiai_(kitas.pazymiai_),
           egzo_rezas_(kitas.egzo_rezas_),
           rezas_vid_(kitas.rezas_vid_),
           rezas_med_(kitas.rezas_med_) {}
     // perkėlimo konstr [Studentas x(std::move(y)) / Studentas x = std::move(y)]
     Studentas(Studentas &&kitas)
-        : vardas_(std::move(kitas.vardas_)),
-          pavarde_(std::move(kitas.pavarde_)),
+        : Zmogus(kitas),
           pazymiai_(std::move(kitas.pazymiai_)),
           egzo_rezas_(kitas.egzo_rezas_), // be std::move, nes paprastiem tipam kaip int/float tai netikslinga (skirtingai nei std::string, std::vector — objektam)
           rezas_vid_(kitas.rezas_vid_),
@@ -53,6 +47,12 @@ public:
         kitas.egzo_rezas_ = 0;
         kitas.rezas_vid_ = 0;
         kitas.rezas_med_ = 0;
+    }
+
+    // destruktorius
+    ~Studentas() override
+    {
+        pazymiai_.clear();
     }
 
     // perkrauti operatoriai
@@ -68,8 +68,8 @@ public:
     // gavikai / getteriai
 
     // inline float rezas() const { return rezas_; }
-    inline std::string vardas() const { return vardas_; }
-    inline std::string pavarde() const { return pavarde_; }
+    // inline std::string vardas() const { return vardas_; }
+    // inline std::string pavarde() const { return pavarde_; }
     inline Container(int) pazymiai() const { return pazymiai_; }
     inline int egzo_rezas() const { return egzo_rezas_; }
     inline float rezas_vid() const { return rezas_vid_; }
@@ -80,9 +80,9 @@ public:
 
     // nustatytojai / setteriai
 
-    void nust_varda(std::string vardas) { vardas_ = vardas; }
-    void nust_pavarde(std::string pavarde) { pavarde_ = pavarde; }
-    bool ivest_varda_pavarde(bool ar_ivestis_atsaukiama = false);
+    // void nust_varda(std::string vardas) { vardas_ = vardas; }
+    // void nust_pavarde(std::string pavarde) { pavarde_ = pavarde; }
+    // bool ivest_varda_pavarde(bool ar_ivestis_atsaukiama = false);
     void pridet_pazymi(int paz);
     void nust_egzo_reza(int rez);
     void ivest_egzo_reza();
--- src/zmogus.h
+++ src/zmogus.h
@@ -1,24 +1,33 @@
+#pragma once
+
+#include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
+
+#include "klaidu_valdymas.h"
 
 class Zmogus
 {
-private:
+protected:
     std::string vardas_;
     std::string pavarde_;
 
 public:
+    Zmogus() : vardas_(""), pavarde_("") {}
+    // surasant rankiniu budu
+    Zmogus(std::string vardas, std::string pavarde) : vardas_(vardas), pavarde_(pavarde) {}
+    // kopijavimo konstr [Zmogus x(y) / Zmogus x = y]
+    Zmogus(const Zmogus &kitas)
+        : vardas_(kitas.vardas_),
+          pavarde_(kitas.pavarde_) {}
+    // perkėlimo konstr [Zmogus x(std::move(y)) / Zmogus x = std::move(y)]
+    Zmogus(Zmogus &&kitas)
+        : vardas_(std::move(kitas.vardas_)),
+          pavarde_(std::move(kitas.pavarde_)) {}
+
+    // VIRTUALUS destruktorius
     virtual ~Zmogus() = 0;
-
-    // perkrauti operatoriai
-
-    // kopijavimo priskyrimo operatorius [x = y]
-    virtual Zmogus &operator=(const Zmogus &kitas) = 0;
-    // perkėlimo priskyrimo operatorius [x = std::move(y)]
-    virtual Zmogus &operator=(Zmogus &&kitas) = 0;
-
-    // friend std::ostream &operator<<(std::ostream &os, const Zmogus &stud);
-    // friend std::istream &operator>>(std::istream &is, Zmogus &stud);
 
     // gavikai / getteriai
 
@@ -29,5 +38,6 @@ public:
 
     void nust_varda(std::string vardas) { vardas_ = vardas; }
     void nust_pavarde(std::string pavarde) { pavarde_ = pavarde; }
+
     bool ivest_varda_pavarde(bool ar_ivestis_atsaukiama = false);
 };
