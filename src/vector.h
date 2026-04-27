c344d398706d1c02e746529722e56903a5fe8d06
ainis997 <235386176+ainis997@users.noreply.github.com>
2026-04-27 19:30:06 +0300

    Sukurti vektoriaus konstruktoriai ir destruktorius.

--- src/vector.h
+++ src/vector.h
@@ -34,19 +34,50 @@ public:
 
     // konstruktoriai
 
+    // numatytasai konstruktorius
     Vector() : data_(nullptr), size_(0), capacity_(0) {}
+    // konstruktorius n elementų vektoriaus
     Vector(size_type n) : size_(n), capacity_(n)
     {
         data_ = new T[n](); // () — daro value-initialization visiem nariam (int: 0, float: 0.0, objektam: default konstruktoriai)
     }
+    // konstruktorius su visų prad. elementų užpildymu elementais x
     Vector(size_type n, const T &x) : size_(n), capacity(n)
     {
         data_ = new T[n];
         std::fill(data_, data_ + n, x); // užpildo masyvą reikšmėmis x iki size_-tojo elemento
     }
+    // konstruktorius su inicializavimo sąrašu
     Vector(std::initializer_list<T> list) : size_(list.size()), capacity_(list.size())
     {
         data_ = new T[n];
         std::copy(list.begin(), list.end(), data_);
+    }
+    // kopijavimo konstruktorius
+    Vector(const Vector &other) : size_(other.size()), capacity_(other.size())
+    {
+        data_ = new T[capacity_];
+        std::copy(other.begin(), other.end(), data_);
+    }
+    // perkėlimo konstruktorius
+    Vector(Vector &&other) : data_(other.data_), size_(other.size()), capacity_(other.size())
+    {
+        // AR TSG DESTRUKTORIUM?
+        other.data_ = nullptr;
+        other.size_ = 0;
+        other.capacity_ = 0;
+    }
+    ///
+
+    ~Vector()
+    {
+        delete[] data_;
+        size_ = 0;
+        capacity_ = 0;
+    }
+
+    size_type size()
+    {
+        return size_;
     }
 };
